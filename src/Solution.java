e47df9da56bbaeca92a5cdb5eea139264c2556e9
Al-Rou <roal.24.alro@gmail.com>
2021-07-18 01:35:13 -0400

    commented

--- src/Solution.java
+++ src/Solution.java
@@ -6,11 +6,13 @@ public class Solution {
     public int solution(int N, int K, int[] A, int[] B, int[] C)
     {
         List<Stack<Integer>> list = new ArrayList<>();
+        //Create stacks as many as N determines, and store them in a list
         for(int i = 0; i < N; i++)
         {
             Stack<Integer> stack= new Stack<>();
             list.add(stack);
         }
+        //Values from C are pushed inside stacks according to the order determined by A and B
         for(int i = 0; i < C.length; i++)
         {
             if(A[i] > B[i])
@@ -23,10 +25,14 @@ public class Solution {
                 list.get(j-1).push(C[i]);
             }
         }
+        //If poped values are not in order (from K to 1), remove that stack from the list, since it is not
+        //a good cake!
         for(int i = K; i > 0; i--)
         {
             for(int j = 0; j < list.size(); j++)
             {
+                //If one stack is empty before i reaches 1, it means that stack does not
+                //represent a good cake, hence removed from the list!
                 if(list.get(j).isEmpty())
                 {
                     list.remove(j);
@@ -49,6 +55,7 @@ public class Solution {
                 }
             }
         }
+        //The size of the list shows the answer
         return list.size();
     }
 }
